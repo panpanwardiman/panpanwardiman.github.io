67e6e68af83ff16ace97826a8f1161f042ad06c0
panpan wardiman <pwardiman@gmail.com>
2021-02-02 10:23:14 +0700

    new design

--- js/app.js
+++ js/app.js
@@ -1,70 +1,68 @@
-$(document).ready(function(){
-  var $body = $('body');
-  var $navbar = $('.navbar');
-  var $offsetY = $navbar.offset().top + 10;
+const bodyEl = document.getElementsByTagName('html')[0]
 
-  // scroll navbar fixed
-  function scroll() {
-    if ($(window).scrollTop() >= $offsetY) {
-      $navbar.addClass('fixed-top').css('background-color', 'rgba(255, 254, 253, 0.97');
-        // console.log('eko');
-    } else {
-      $navbar.removeClass('fixed-top').css('background-color', 'transparent');
-        // console.log('oke');
+const openNav = () => {
+  const nav = document.querySelector(".mobile-menu-wrapper")
+  nav.classList.add("open")
+}
+const closeNav = () => {
+  const nav = document.querySelector(".mobile-menu-wrapper")
+  nav.classList.remove("open")
+}
+
+const LOCAL_STORAGE_KEY = 'user-theme-scheme'
+const THEME_KEY = '--code-mode'
+const modeToggleButton = document.querySelectorAll('#btn-dark-mode')
+
+/**
+ * getCodeModeProp untuk mencari value property --code-mode di document root element 
+ * atau tag html 
+ */
+const getCodeModeProp = (propKey) => {
+    let codeMode = getComputedStyle(document.documentElement).getPropertyValue(propKey)
+    if (codeMode.length) {
+        codeMode = codeMode.replace(/\'|"/g, '').trim()
     }
-  }
+    return codeMode
+}
 
-  document.onscroll = scroll;
-});
-
-// smooth scroll
-$(function() {
-  $('a[href*=\\#]:not([href=\\#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 700);
-        return false;
-      }
+/**
+ * 
+ * @param {*} mode 
+ */
+const modeSetting = mode => {
+    let currentSetting = mode || localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (currentSetting) {
+      document.documentElement.setAttribute('data-theme', currentSetting)
     }
-  });
-});
+}
 
-// window scroll animation
-$(window).scroll(function(e){
-  e.preventDefault();
-  var wScroll = $(this).scrollTop();
-
-  console.log(wScroll);
-
-  if (wScroll > $('#about').offset().top - 200) {
-    $('#about .content').addClass('show');
-    $('#about .text-center').addClass('animated fadeInDown').css('animation-delay', '0.6s');
-    $('#about .skill').addClass('animated fadeInUp').css('animation-delay', '0.6s');
-    $('.skill-bar').each(function(){
-      $(this).find('.skill-bar-bar').animate({
-        width:jQuery(this).attr('data-percent')
-      },2500);
-    });
+const toggleSetting = () => {
+  let currentSetting = localStorage.getItem(LOCAL_STORAGE_KEY);
+  
+  switch(currentSetting) {
+    case null:
+      currentSetting = getCodeModeProp(THEME_KEY) === 'dark' ? 'light' : 'dark';
+      break;
+    case 'light':
+      currentSetting = 'dark';
+      break;
+    case 'dark':
+      currentSetting = 'light';
+      break;
   }
 
-  if (wScroll > $('#portfolio').offset().top - 200) {
-    $('#portfolio .container').addClass('show');
-    $('#portfolio .text-center').addClass('animated fadeInDown').css('animation-duration', '2s');
-    $('#portfolio .box').addClass('animated bounceInDown').css('animation-duration', '2s');
-  }
+  localStorage.setItem(LOCAL_STORAGE_KEY, currentSetting);
+  
+  return currentSetting;
+}
 
-  if (wScroll > $('#contact').offset().top - 200) {
-    $('#contact .container').addClass('show');
-    $('#contact .text-center').addClass('animated fadeInDown').css('animation-duration', '2s');
-    $('#contact .row').addClass('animated flipInX').css('animation-duration', '2s');
-  }
+modeToggleButton.forEach(item => {
+  item.addEventListener('click', e => {
+    e.preventDefault()
+    modeSetting(toggleSetting())
+  })
+})
 
-});
-
-
+modeSetting();
 
 
